58de814929ed4be0ce3c16a08239f57f6c8a51bf
Lucas Maia <lucas.mdantas@icloud.com>
2023-12-12 08:48:20 -0300

    Validacoes na funcao e otimizacao do script

--- functions.py
+++ functions.py
@@ -2,11 +2,52 @@ import pyperclip
 import pyautogui as bot
 
 def preencher_produto(produto):
-    bot.sleep(0.5)
+    '''
+    Preenche os campos do sistema web com as informações do produto passado
+    A função verifica sempre o primeiro item de cada página, esperando com que a página carregue totalmente para preencher as informações corretamente
+
+    Parameters:
+        produto (dict): dicionário contendo as informações do produto
+
+    '''
     for chave, valor in produto.items():
-        pyperclip.copy(valor)
-        bot.hotkey('ctrl', 'v')
-        bot.press('tab')
-        bot.press('enter')
+        bot.sleep(0.5)
+        if chave == 'Descrição':
+            pyperclip.copy(valor)
+            bot.hotkey('ctrl', 'v')
+            bot.press('tab')
+        elif chave == 'Preço':
+            bot.sleep(1)
+            bot.press('tab')
+            pyperclip.copy(valor)
+            bot.hotkey('ctrl', 'v')
+            bot.press('tab')
+        elif chave == 'Fabricante':
+            bot.sleep(1)
+            bot.press('tab')
+            pyperclip.copy(valor)
+            bot.hotkey('ctrl', 'v')
+            bot.press('tab')
+        elif chave == 'Tamanho':
+            bot.press('enter')
+            if valor == 'Grande':
+                bot.press('g')
+                bot.press('enter')
+            elif valor == 'Médio':
+                bot.press('m')
+                bot.press('enter')
+            else:
+                bot.press('p')
+                bot.press('enter')
+            bot.press('enter')
+            bot.press('tab')
+        else:
+            pyperclip.copy(valor)
+            bot.hotkey('ctrl', 'v')
+            bot.press('tab')
+            bot.press('enter')
+
+
+        
 
     
--- script.py
+++ script.py
@@ -12,11 +12,13 @@ import openpyxl
 # pyautogui -> automação click e teclado
 # openpyxl -> automatizar e ler planilhas
 
+site = 'https://cadastro-produtos-devaprender.netlify.app/'
+
 bot.hotkey('win', 's')
 bot.write('chrome')
 bot.press('enter')
 bot.sleep(2)
-bot.write('https://cadastro-produtos-devaprender.netlify.app/')
+bot.write(site)
 bot.press('enter')
 bot.sleep(2)
 bot.press('tab')
@@ -24,36 +26,18 @@ bot.press('tab')
 workbook = openpyxl.load_workbook('produtos_ficticios.xlsx')
 sheet_produtos = workbook['Produtos']
 
-produto = {}
-for linha in sheet_produtos.iter_rows(min_row=2):
-    produto['nome_produto'] = linha[0].value
-    produto['descricao'] = linha[1].value
-    produto['categoria'] = linha[2].value
-    produto['codigo'] = linha[3].value
-    produto['peso_kg'] = linha[4].value
-    produto['dimensoes'] = linha[5].value
-    produto['preco'] = linha[6].value
-    produto['qtdade'] = linha[7].value
-    produto['validade'] = linha[8].value
-    produto['cor'] = linha[9].value
-    produto['tamanho'] = linha[10].value
-    produto['material'] = linha[11].value
-    produto['fabricante'] = linha[12].value
-    produto['pais_origem'] = linha[13].value
-    produto['obs'] = linha[14].value
-    produto['codigo_barras'] = linha[15].value
-    produto['loc_estoque'] = linha[16].value
 
-preencher_produto(produto)
-
-
-
-
-
-
-
-
-
-
+column_names = [cell.value for cell in sheet_produtos[1]]
+for row in sheet_produtos.iter_rows(min_row=2, values_only=True):
+    produto = {}
+    for index, column in enumerate(column_names):
+        produto[column] = row[index]
+    preencher_produto(produto)
+    bot.press('enter')
+    bot.sleep(1)
+    bot.press('tab')
+    bot.press('enter')
+    bot.sleep(1)
+    bot.press('tab')        
 
 
